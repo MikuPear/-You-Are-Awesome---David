e914fcbc0c8d5dda4df71a5490c4120c074cc1a8
David Garcia-Martinez <joshuagarcia_12@yahoo.com>
2025-11-03 13:28:59 -0800

    Initial Commit

---
+++ YourAreAwesome/YourAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourAreAwesomeApp.swift
+//  YourAreAwesome
+//
+//  Created by GARCIA-MARTINEZ, DAVID on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct YourAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
